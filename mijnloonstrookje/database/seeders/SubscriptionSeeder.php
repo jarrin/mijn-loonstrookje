686f984c45573edf2bfdd55b7073f1950e019ac2
Nick <nickcolijn2006@gmail.com>
2025-10-28 12:09:43 +0100

    subscribtion features

--- mijnloonstrookje/database/seeders/SubscriptionSeeder.php
+++ mijnloonstrookje/database/seeders/SubscriptionSeeder.php
@@ -15,19 +15,25 @@ class SubscriptionSeeder extends Seeder
         $subscriptions = [
             [
                 'name' => 'simpel plan',
-                'description' => 'Simpel subscription plan with basic features',
+                'feature_1' => 'Tot 5 werknemers',
+                'feature_2' => 'Automatische loonstroken',
+                'feature_3' => 'Email ondersteuning',
                 'price' => 9.99,
                 'subscription_plan' => 'simple',
             ],
             [
                 'name' => 'basic plan',
-                'description' => 'Basic subscription with essential features',
+                'feature_1' => 'Tot 25 werknemers',
+                'feature_2' => 'Automatische loonstroken en belastingdocumenten',
+                'feature_3' => 'Prioriteit email ondersteuning',
                 'price' => 29.99,
                 'subscription_plan' => 'basic',
             ],
             [
                 'name' => 'premium plan',
-                'description' => 'Premium subscription with advanced features and priority support',
+                'feature_1' => 'Onbeperkt werknemers',
+                'feature_2' => 'Volledige automatisering en rapportages',
+                'feature_3' => '24/7 telefoon en email ondersteuning',
                 'price' => 99.99,
                 'subscription_plan' => 'premium',
             ],
