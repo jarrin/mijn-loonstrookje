a89c9063831427bc904ef5c83040ca5a4e7da528
Job <Jobisa12@gmail.com>
2025-11-06 10:15:40 +0100

    basis

--- mijnloonstrookje/resources/views/SuperAdminDashboard.blade.php
+++ mijnloonstrookje/resources/views/SuperAdminDashboard.blade.php
@@ -6,6 +6,30 @@
 <section>
     <h1 class="text-2xl mb-4">Super Admin Dashboard</h1>
     <p>Welkom {{ auth()->user()->name }}, je hebt volledige toegang tot het systeem.</p>
+
+    <table>
+        <thead>
+            <tr>
+                <th>Gebruiker</th>
+                <th>Bedrijf</th>
+                <th>Email</th>
+                <th>Rol</th>
+                <th>Status</th>
+                <th class="icon-cell">Acties</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Jan Jansen</td>
+                <td>DMG</td>
+                <td>jansen@dmg.nl</td>
+                <td>Admin</td>
+                <td>Actief</td>
+                <td class="icon-cell">{!! file_get_contents(resource_path('assets/icons/trashbin.svg')) !!}</td>
+            </tr>
+        </tbody>
+    </table>
+      
     
     <div class="mt-6 space-x-4">
         <a href="{{ route('superadmin.subscriptions') }}" class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Abonnementen</a>
