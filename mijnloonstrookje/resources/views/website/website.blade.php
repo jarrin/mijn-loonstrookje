55de5f1411c00fd7934870b0fc9e9aa3e15cc2da
job <Jobisa12@gmail.com>
2026-01-14 09:11:37 +0100

    Alle blokken netjes gecentreerd

--- mijnloonstrookje/resources/views/website/website.blade.php
+++ mijnloonstrookje/resources/views/website/website.blade.php
@@ -63,10 +63,7 @@
                 </div>
                 <div class="feature-card">
                     <div class="feature-icon">
-                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M12 2C6.5 2 2 6.5 2 12C2 17.5 6.5 22 12 22C17.5 22 22 17.5 22 12C22 6.5 17.5 2 12 2Z" stroke="#0052cc" stroke-width="2" fill="none"/>
-                            <path d="M8 12L11 15L16 8" stroke="#0052cc" stroke-width="2" stroke-linecap="round"/>
-                        </svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#0052cc" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-download-icon lucide-download"><path d="M12 15V3"/><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/><path d="m7 10 5 5 5-5"/></svg>
                     </div>
                     <h3>Eenvoudig Downloaden</h3>
                     <p>Download loonstroken individueel of in bulk. Perfect voor loonbrieven en archivering.</p>
@@ -84,21 +81,27 @@
             <div class="admin-content">
                 <div class="admin-features">
                     <div class="admin-feature">
-                        <span class="admin-icon">👥</span>
+                        <span class="admin-icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#0052cc" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-building2-icon lucide-building-2"><path d="M10 12h4"/><path d="M10 8h4"/><path d="M14 21v-3a2 2 0 0 0-4 0v3"/><path d="M6 10H4a2 2 0 0 0-2 2v7a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2h-2"/><path d="M6 21V5a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v16"/></svg>
+                        </span>
                         <div>
                             <h4>Multi-tenant beheer</h4>
                             <p>Beheer meerdere bedrijven vanuit één account</p>
                         </div>
                     </div>
                     <div class="admin-feature">
-                        <span class="admin-icon">🔐</span>
+                        <span class="admin-icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#0052cc" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-users-icon lucide-users"><path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2"/><path d="M16 3.128a4 4 0 0 1 0 7.744"/><path d="M22 21v-2a4 4 0 0 0-3-3.87"/><circle cx="9" cy="7" r="4"/></svg>
+                        </span>
                         <div>
                             <h4>Centrale ingang</h4>
                             <p>Sluit klanten in en geef hen volledige Hinzugsystemen</p>
                         </div>
                     </div>
                     <div class="admin-feature">
-                        <span class="admin-icon">📊</span>
+                        <span class="admin-icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#0052cc" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chart-column-icon lucide-chart-column"><path d="M3 3v16a2 2 0 0 0 2 2h16"/><path d="M18 17V9"/><path d="M13 17V5"/><path d="M8 17v-3"/></svg>
+                        </span>
                         <div>
                             <h4>Uitgebreide rapportage</h4>
                             <p>Inzicht in gebruik en activiteit per klant</p>
